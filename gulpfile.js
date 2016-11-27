e04597ac258da12302ac520824c040b2b5801c62
Akshay Nair <phenax5@gmail.com>
2016-11-27 15:24:28 +0530

    Image optimization done

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,66 @@
 const gulp= require('gulp');
 const minify= require('gulp-clean-css');
+const imageOptim= require('gulp-image-optimization');
+const imageResize= require('gulp-image-resize');
+const rename= require('gulp-rename');
 const url= require('url');
 
-const BUILD_DIR= "./css";
-const FILE_DIR= "./css/min";
 
+// Options to pass to gulp-image-optimization
+const optimizationOptions= {
+	optimizationLevel: 5,
+	progressive: true,
+};
+
+// All the images to optimize
+const imageList= [
+	{
+		name: '2048.png',
+		location: 'img',
+		dest: './dist/img',
+		size: 300
+	},
+	{
+		name: 'cam_be_like.jpg',
+		location: 'img',
+		dest: './dist/img',
+		size: 300
+	},
+	{
+		name: 'mobilewebdev.jpg',
+		location: 'img',
+		dest: './dist/img',
+		size: 300
+	},
+	{
+		name: 'profilepic.jpg',
+		location: 'img',
+		dest: './dist/img',
+		size: 300
+	}
+];
 
 function minfyCSS() {
+
 	return gulp
-		.src(`${FILE_DIR}/*.css`)
-		.pipe(sass())
-		.pipe(autoprefixer())
+		.src(`./css/*.css`)
 		.pipe(minify({ compatibility: 'ie8' }))
-		.pipe(gulp.dest(`${BUILD_DIR}/css/`));
+		.pipe(gulp.dest('./dist/css'));
 }
 
+/**
+ * Optimizes all the images
+ */
 function minifyImages() {
 
+	imageList.forEach(
+		image => 
+			gulp.src(`./${image.location}/${image.name}`)
+				.pipe(imageResize({ width: image.size }))
+				// .pipe(imageOptim(optimizationOptions))
+				.pipe(gulp.dest(image.dest))
+	);
 }
 
 gulp.task('minify:css', minfyCSS);
 gulp.task('minify:img', minifyImages);
-
-gulp.task('default', ['minify:css']);
