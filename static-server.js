7aadccaad877122ab9983c4cffb8a7478bf349f6
Akshay Nair <phenax5@gmail.com>
2016-11-28 17:29:22 +0530

    Server done with gzip enabled

--- static-server.js
+++ static-server.js
@@ -1,91 +1,135 @@
 
 const http= require('http');
 const path= require('path');
+const zlib= require('zlib');
 const fs= require('fs');
 const url= require('url');
+const mimeTypes= require('mime');
 
 
-http.createServer((req, res) => {
+/**
+ * 404 Error Handler
+ */
+function errorHandler(res) {
+	res.writeHead(404, {'Content-Type': 'text/plain'});
+	res.end('404 Not Found\n');
+}
 
-	const uri= url.parse(req.url).pathname;
-	const filePath = path.join(process.cwd(), unescape(uri));
-
-	console.log(req.url, uri, filePath);
+/**
+ * Check if a file exists
+ * 
+ * @param  {String} filePath    The path name of the file
+ * 
+ * @return {FileStat|Boolean}   The file stats
+ */
+function resolvePath(filePath) {
 
 	let fileStat;
 
 	try {
 		fileStat = fs.lstatSync(filePath);
 	} catch (e) {
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.end('404 Not Found\n');
-		return;
+		return false;
 	}
 
-	if(fileStat && fileStat.isFile()) {
+	return fileStat;
+}
 
+
+/**
+ * If encoding is supported, encodes a file 
+ * stream with gzip or deflates it.
+ * 
+ * @param  {FileStream} file$  Readable file stream
+ * @param  {Request}    req    The request object
+ * @param  {Response}   res    The response object
+ * 
+ * @return {Object}            The stream and the encoding type in an object
+ */
+function gzipStream(file$, req) {
+
+	// Check if the client supports gzip or deflate encoding
+	const encType= req.headers['accept-encoding'].match(/(gzip|deflate)/i);
+
+	// If the client doesnt support gzip or deflate, 
+	// return the stream as it is
+	if(encType === null)
+		return {
+			stream$: file$,
+			encoding: null
+		};
+
+	const compression$= 
+		(encType[0] === 'deflate')? 
+			zlib.createDeflate(): 
+			zlib.createGzip();
+
+	return {
+		stream$: file$.pipe(compression$),
+		encoding: encType[0]
+	}
+}
+
+
+/**
+ * Server request handler
+ */
+function serverRequestHandler(req, res) {
+
+	console.log("Fetching " + req.url);
+
+	const uri= url.parse(req.url).pathname;
+	let filePath = path.join(process.cwd(), unescape(uri));
+
+	// Get file stats
+	const fileStat= resolvePath(filePath);
+
+	// If the file exists
+	if(fileStat) {
+
+		// If its a directory
+		if(fileStat.isDirectory()) {
+
+			// Append index.html to the path
+			filePath= path.join(filePath, 'index.html');
+
+			// Check if the file exists
+			const stat= resolvePath(filePath);
+
+			// If it doesnt, 404!!
+			if(!(stat && stat.isFile())) {
+				return errorHandler(res);
+			}
+		}
+
+		// Get the mime type of the file from the filePath
+		const mime= mimeTypes.lookup(filePath);
+
+		// Create a readable stream from the file
 		const file$= fs.createReadStream(filePath);
 
-		console.log(fs.readFileSync(filePath).toString());
+		if(file$) {
 
-		file$.pipe(res);
+			// Enable compression for the file stream
+			const compressed= gzipStream(file$, req);
+
+			// If the stream was encoded, set the header
+			if(compressed.encoding)
+				res.setHeader('Content-Encoding': compressed.encoding);
+
+			res.writeHead(200, { 'Content-Type': mime });
+
+			// Pipe it out to the response
+			compressed.stream$.pipe(res);
+		} else
+			errorHandler(res);
+
+	} else {
+		errorHandler(res);
 	}
+}
 
-}).listen(8080, ()=> {
+
+http.createServer(serverRequestHandler).listen(8080, ()=> {
 	console.log("Server started");
 });
-
-
-
-/*
-var http = require('http'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs');
-var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"};
-
-http.createServer(function(req, res) {
-  var uri = url.parse(req.url).pathname;
-  var filename = path.join(process.cwd(), unescape(uri));
-  var stats;
-
-  try {
-    stats = fs.lstatSync(filename); // throws if path doesn't exist
-  } catch (e) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.write('404 Not Found\n');
-    res.end();
-    return;
-  }
-
-
-  if (stats.isFile()) {
-    // path exists, is a file
-    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
-    res.writeHead(200, {'Content-Type': mimeType} );
-
-    var fileStream = fs.createReadStream(filename);
-    fileStream.pipe(res);
-  } else if (stats.isDirectory()) {
-    // path exists, is a directory
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Index of '+uri+'\n');
-    res.write('TODO, show index?\n');
-    res.end();
-  } else {
-    // Symbolic link, other?
-    // TODO: follow symlinks?  security?
-    res.writeHead(500, {'Content-Type': 'text/plain'});
-    res.write('500 Internal server error\n');
-    res.end();
-  }
-
-}).listen(1337);
-
- */
