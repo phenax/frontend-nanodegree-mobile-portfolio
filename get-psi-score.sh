aab8d32d77e44d1e72df157f5bfdebc4d1c5ccee
Akshay Nair <phenax5@gmail.com>
2016-11-28 18:17:27 +0530

    Page speed insights score updated

--- get-psi-score.sh
+++ get-psi-score.sh
@@ -12,9 +12,16 @@ echo -e "FEND Project PSI scores" >> scores.txt;
 
 
 addSeperator;
-
 psi $1 >> scores.txt;
 
 addSeperator;
+psi $1/project-2048.html >> scores.txt
 
-psi $1/project-2048 >> scores.txt
+addSeperator;
+psi $1/project-mobile.html >> scores.txt
+
+addSeperator;
+psi $1/project-webperf.html >> scores.txt
+
+addSeperator;
+psi $1/pizza.html >> scores.txt
